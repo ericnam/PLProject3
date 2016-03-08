e618c39e6b37e7897277d6fd91d84d174a23f6ed
ericnam <ericnam86@utexas.edu>
2016-03-07 21:58:17 -0600

    able to switch pics and caption

--- image_gallery/image_gallery.js
+++ image_gallery/image_gallery.js
@@ -1,14 +1,22 @@
 $(document).ready(function (){
     $('a').each(function() {
         var URL = $(this).attr('href');
+
         //preload
         var img = new Image();
         img.src = URL;
 
         var caption = $(this).attr('title');
-
-        $('a').click(function (evt) {
-            $(document).getElementById('caption').value = $(this).attr('title');
-        })
     });
+
+    $('a').click(function (event) {
+        //prevent link from redirect
+        event.preventDefault();
+
+        var caption = (document).getElementById('caption');
+        caption.innerHTML = $(this).attr('title');
+
+        var picture = document.getElementById('image');
+        picture.src = $(this).attr('href');
+    })
 })
